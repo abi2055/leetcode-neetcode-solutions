e64a919dbbe2ea97da30bdd40cd6415c59b91277
Abishek Naathan <93240293+abi2055@users.noreply.github.com>
2026-04-28 22:57:07 -0400

    [141] [Time Beats: 13.16%] [Memory Beats: 30.40%] - LeetPush

--- Linked-List-Cycle.py
+++ Linked-List-Cycle.py
@@ -10,16 +10,29 @@
 10        :type head: ListNode
 11        :rtype: bool
 12        """
-13         # hashset method
+13        # hashset method
 14        current = head
 15
-16        visited = set()
+16        # visited = set()
 17
-18        while current:
-19            if current in visited:
-20                return True
-21            visited.add(current)
-22            current = current.next
+18        # while current:
+19        #     if current in visited:
+20        #         return True
+21        #     visited.add(current)
+22        #     current = current.next
 23        
-24        return False
-25        
+24        # return False
+25
+26        # fast slow pointer
+27
+28        fp = head
+29        sp = head
+30
+31        while fp and fp.next:
+32            fp = fp.next.next
+33            sp = sp.next
+34            if fp == sp:
+35                return True
+36
+37        return False
+38        
